9bf12f8417eb1a44a465937f695e22439c302d4e
stdimitrov05 <stdimitrov05@gmail.com>
2023-04-30 14:32:51 +0300

    done routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,10 @@
 module.exports = {
     'home': '/',
-    'login': '/login',
-    'signup': '/signup',
+    'aboutUs': '/about-us',
+    // Services Dropdown
+    'airportTrans': '/airport-transfers',
+    'rentWithDrive': '/rent-driver',
+    'tours': '/tours',
+    'blog': '/blog',
+    'contact': '/contact',
 }
