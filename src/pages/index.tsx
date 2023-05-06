e677e62ba8a9612520b242fc7bd769facbe35c58
mony <ivanovasimona787@gmail.com>
2023-05-06 18:14:24 +0300

    Finished banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Navigation from "@/pages/components/common/Navigation";
+import { FaChevronRight } from "react-icons/fa";
 export default function Home() {
   return (
       <div className={"main-container"}>
@@ -13,9 +14,28 @@ export default function Home() {
                           <br/>
                           Of Here!
                       </p>
+                      <div className={"transfers-btn"}>
+                              <FaChevronRight className={"button-circle"} size={25} color={"#B4B4B4"}/>
+                          <p className={"transfers"}>Transfers</p>
+                      </div>
                   </div>
               </div>
-
+              <div className={"banner-container"}>
+                  <div className={"banner-content"}>
+                      <div className={"experience"}>
+                          <p className={"sub-experience"}>5+</p>
+                          <p className={"description"}>Years experience</p>
+                      </div>
+                      <div className={"destination"}>
+                          <p className={"sub-destination"}>10+</p>
+                          <p className={"destination-collab"}>Destination Collaboration</p>
+                      </div>
+                      <div className={"tourist-dest"}>
+                          <p className={"sub-tourist"}>50+</p>
+                          <p className={"tourist-destination"}>Tourist Destination</p>
+                      </div>
+                  </div>
+              </div>
           </div>
       </div>
   )
